f136865d82feeeaffc272accf031f0216b59d9f3
WoolyDrool <nikolai.mcneely15@gmail.com>
2022-11-30 07:26:19 -0800

    Fixed incorrect class declaration

--- entities/player/tools/tool.gd
+++ entities/player/tools/tool.gd
@@ -1,32 +1,29 @@
 # Base class defining core functionality of all usable tools
 # Handles boilerplate like input checking and some basic control booleans
+class_name Tool
+	
+var toolName = "Default"
+var isEquip : bool = false
+var canUSe : bool = true
 
-extends Node3D
+func _process(delta):
+	pass
 
-class Tool:
-	
-	var toolName = "Default"
-	var isEquip : bool = false
-	var canUSe : bool = true
-	
-	func _process(delta):
-		pass
-	
-	func _process_input():
-		if Input.is_action_just_pressed("tool_primary"):
-			_tool_primary()
-		if Input.is_action_just_pressed("tool_secondary"):
-			_tool_secondary()
-		if Input.is_action_just_pressed("tool_tertiary"):
-			_tool_tertiary()
-		pass
-	
-	# Actions
-	func _tool_primary():
-		pass
-	
-	func _tool_secondary():
-		pass
-	
-	func _tool_tertiary():
-		pass
+func _process_input():
+	if Input.is_action_just_pressed("tool_primary"):
+		_tool_primary()
+	if Input.is_action_just_pressed("tool_secondary"):
+		_tool_secondary()
+	if Input.is_action_just_pressed("tool_tertiary"):
+		_tool_tertiary()
+	pass
+
+# Actions
+func _tool_primary():
+	pass
+
+func _tool_secondary():
+	pass
+
+func _tool_tertiary():
+	pass
