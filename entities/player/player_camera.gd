4b696cee4feb453fb968242477688435175154ca
WoolyDrool <nikolai.mcneely15@gmail.com>
2022-12-01 02:08:28 -0800

    Made interact raycast logic a bit more plain to understand

--- entities/player/player_camera.gd
+++ entities/player/player_camera.gd
@@ -16,14 +16,19 @@ func _ready():
 func _process(delta):
 	if raycaster.is_colliding():
 		x = raycaster.get_collider()
-		if x.has_method("_on_grab"):
-			interactLabel.set_text("Grab " + str(x))
-			canGet = true
-	else:
-		interactLabel.set_text("")
-		canGet = false
 
-	if Input.is_action_just_pressed("interact_general") && canGet:
-		if x != null:
-			x._on_grab()
-			x = null
+		if x:
+			if x.has_method("_on_grab"):
+				canGet = true
+			else:
+				canGet = false
+			
+			if canGet:
+				interactLabel.text = ("Grab " + str(x))
+				if Input.is_action_just_pressed("interact_general"):
+					x._on_grab()
+					x.queue_free()
+					x = null
+					canGet = false
+	else:
+		interactLabel.text = ""
