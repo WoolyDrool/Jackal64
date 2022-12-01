6cbf9e49e7c56e1513d50c714f11e43aa804d936
WoolyDrool <nikolai.mcneely15@gmail.com>
2022-11-30 19:05:02 -0800

    Tried fiddling with some signal solutions

--- entities/objective_piece.gd
+++ entities/objective_piece.gd
@@ -1,11 +1,14 @@
 extends Area3D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#EventBus.connect("was_collected", self, )
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_grab():
+	EventBus.emit_signal("was_collected")
