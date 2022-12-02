21d3e920e9db51eeb39a08316e92aee5468c4ab7
WoolyDrool <nikolai.mcneely15@gmail.com>
2022-12-01 22:33:06 -0800

    Commented a method and connected a signal

--- systems/TrailTrashContainer.gd
+++ systems/TrailTrashContainer.gd
@@ -4,8 +4,12 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Will connect this via a signal function call later for easier initialisation
+	# It probably will fail to load occasionally cause of variances in load order but
+	# ¯\_(ツ)_/¯ 
 	itemsInTree = get_child_count()
-	
+	print(itemsInTree)
+	EventBus.G_T_UPDATE_OBJECTIVE_COUNT.emit(itemsInTree)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
