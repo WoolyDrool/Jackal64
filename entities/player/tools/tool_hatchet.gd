0b553243c91499304395631ea1e37859e4347844
WoolyDrool <nikolai.mcneely15@gmail.com>
2022-12-02 01:34:54 -0800

    Added basic chopping functionality to hatchet

--- entities/player/tools/tool_hatchet.gd
+++ entities/player/tools/tool_hatchet.gd
@@ -4,9 +4,14 @@ func _ready():
 	_get_nodes()
 
 func _tool_primary() -> void:
+	print("fired")
 	canPrimary = false
 	if primaryActionTimer:
 		primaryActionTimer.start()
+	
+	if ray.get_collider():
+		ray.get_collider()._on_chop()
+	
 
 func _on_tool_primary_cooldown_timeout():
 	canPrimary = true
