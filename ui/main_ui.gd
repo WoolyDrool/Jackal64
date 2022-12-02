e4b3d0dbbf0a5835a6484d138ccab42043a19984
WoolyDrool <nikolai.mcneely15@gmail.com>
2022-12-01 22:35:18 -0800

    Hooked up new labels

--- ui/main_ui.gd
+++ ui/main_ui.gd
@@ -1,19 +1,33 @@
 extends Control
 
-var trashCount : int = 0
-var recycleCount : int = 0
-
 var trashLabel
 var recLabel
+var totalLabel
+var collectLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	trashLabel = $"TrashCount"
 	recLabel = $"RecCount"
-	
-	EventBus.E_O_COLLECT_TRASH.connect(_update_label_trash)
-	EventBus.E_O_COLLECT_RECYCLE.connect(_update_label_recycle)
+	totalLabel = $"TotalCount"
+	collectLabel = $"CollectedCount"
+
+	EventBus.G_UI_UPDATE_COUNTS.connect(_update_counts)
+	#EventBus.E_O_COLLECT_TRASH.connect(_update_label_trash)
+	#EventBus.E_O_COLLECT_RECYCLE.connect(_update_label_recycle)
 	pass # Replace with function body.
+
+func _update_counts():
+	_update_label_trash()
+	_update_label_recycle()
+
+	var _UItotalCollected = (PlayerInventory.level_trash_count + PlayerInventory.level_recycle_count)
+	var _UItotalInTrail = TrailManager.totalItemsInTrail
+
+	totalLabel.text = str(_UItotalInTrail)
+	collectLabel.text = str(_UItotalCollected)
+
+	pass
 
 func _update_label_trash() -> void:
 	trashLabel.text = "Trash - " + str(PlayerInventory.level_trash_count)
