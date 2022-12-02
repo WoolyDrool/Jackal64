f6de290ac6e68dfc4e70e1c543d172a8daec2da7
WoolyDrool <nikolai.mcneely15@gmail.com>
2022-12-02 01:35:06 -0800

    Added a health and chop function

--- entities/objects/choppable_object.gd
+++ entities/objects/choppable_object.gd
@@ -1,5 +1,6 @@
 extends Area3D
 
+var health : int = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +10,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_chop():
+	health -= 1
+	print("chopped")
+	if health == 0:
+		_chop_log()
+
+func _chop_log():
+	# Will eventually have it do things here
+	queue_free()
