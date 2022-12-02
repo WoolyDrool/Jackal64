05b965d248e24ae40ce7ccbdf64608782f265803
WoolyDrool <nikolai.mcneely15@gmail.com>
2022-12-01 21:11:45 -0800

    Removed unused lines

--- entities/objects/objective_piece.gd
+++ entities/objects/objective_piece.gd
@@ -5,16 +5,6 @@ class_name GameItemObjective
 @export var itemName : String = "Default"
 @export var recycling : bool = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	#EventBus.connect("was_collected", self, )
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _on_grab():
 	if !recycling:
 		EventBus.E_O_COLLECT_TRASH.emit()
