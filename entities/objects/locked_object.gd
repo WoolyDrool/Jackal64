68e82ac624ad1f0c034a4321c7f576ae6c4cbaee
WoolyDrool <nikolai.mcneely15@gmail.com>
2022-12-05 20:56:15 -0800

    Added control boolean and finished method

--- entities/objects/locked_object.gd
+++ entities/objects/locked_object.gd
@@ -1,8 +1,12 @@
 extends Node3D
 
-
+@export var locked : bool
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	EventBus.G_UI_MG_LOCK_END.connect(_finished)
+
+	if locked:
+		$"Interact".modifierText = "Locked"
 	pass # Replace with function body.
 
 
@@ -12,3 +16,7 @@ func _process(delta):
 
 func _unlocked():
 	EventBus.G_UI_MG_LOCK_START.emit()
+
+func _finished():
+	$"Interact".modifierText = "Unlocked"
+
