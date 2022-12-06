1505fe7261094c4c7b3a4da48cc9a794c1afcb39
WoolyDrool <nikolai.mcneely15@gmail.com>
2022-12-05 21:25:24 -0800

    Added basic deposit and interact text switching

--- entities/objects/deposit_point.gd
+++ entities/objects/deposit_point.gd
@@ -1,11 +1,36 @@
 extends Area3D
 
+enum depositType {GARB, REC, ALL}
+@export var DepositType : depositType
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	match typeof(DepositType):
+		0:
+			$"Interact".interactText = "Garbage Deposit"
+		1:
+			$"Interact".interactText = "Recycle Deposit"
+		2:
+			$"Interact".interactText = "All Deposit"
+		_:
+			$"Interact".interactText = "Depositor"
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _deposit():
+	match typeof(DepositType):
+		0:
+			$"Interact".interactText = "Garbage Deposit"
+			print("Garbage Deposited")
+		1:
+			$"Interact".interactText = "Recycle Deposit"
+			print("Recycle Deposited")
+		2:
+			$"Interact".interactText = "All Deposit"
+			print("All Deposited")
+		_:
+			print("No deposit type specified")
