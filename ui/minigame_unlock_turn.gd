5a663f8794c135d7c6726b28556f05af0709fc67
WoolyDrool <nikolai.mcneely15@gmail.com>
2022-12-05 19:57:30 -0800

    Tried to get the rotation for the lock working properly, but still struggling. Commented the current process

--- ui/minigame_unlock_turn.gd
+++ ui/minigame_unlock_turn.gd
@@ -11,12 +11,15 @@ var winRotation
 var isHeld : bool = false
 var hasWon : bool = false
 var canHold : bool = false
+var upY : float
+var upX : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	keyholeSprite = $"SprMgLockBottomKeyhole"
 	rotationHelper = $"RotationHelper"
 
+	# 1 & 2
 	startingRotation = keyholeSprite.rotation
 	winRotation = (startingRotation + 90)
 	pass # Replace with function body.
@@ -24,21 +27,53 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# Still trying to figure out the best way to rotate the lock
 	if !hasWon:
 		if isHeld:
-			# Still trying to figure out the best way to rotate the lock
-			var vecToCenter = get_global_mouse_position() - rotationHelper.position
-			var angle = global_position.direction_to(vecToCenter)
-			var dist = global_position.distance_to(clickPoint)
-			keyholeSprite.rotation = (atan2(angle.y, angle.x) / dist) * 100
+			# 1 When the scene is loaded, startingRotation is populated by keyholeSprite's rotation
+			# 2 Then, winRotation is defined based off of startingRotation
+			# 3 When the player clicks on the lock, it stores the mouse position at the frame of the click in the variable clickPoint
+			#	Additionally, it stores the additional click and drag movement of the mouse in the variable upX/Y
+			# 4 Then, the child rotationHelper node has its position set to the clickPoint
+			# 5 MATH
+			# 6 Then keyholeSprite has its rotation augmented, and finally the cursor is warped to the clickPoint
+
+			#var adj = Vector2(clickPoint.x, clickPoint.y)
+			#keyholeSprite.rotation = startingRotation + adj
+			
+			# 4
+			rotationHelper.position = clickPoint
+
+			# 5
+			# The sum difference of clickPoints - upY
+			var difference = rotationHelper.position - Vector2(upX, upY)
+			var floatDif : float = difference.x - difference.y
+			clickPoint + difference
+			keyholeSprite.rotation = startingRotation + floatDif / 3.14
+
+			
+			#var vecToCenter = rotationHelper.position
+			#var angle = global_position.direction_to(vecToCenter)
+			#var dist = global_position.distance_to(angle)
+			#var finalRot = startingRotation + upY / dist * winRotation
+			#clickPoint = Vector2(clickPoint.x + upX, clickPoint.y + upY)
+			
+			# 6
+			#keyholeSprite.rotation = startingRotation + (atan2(angle.y, angle.x)) / dist
+			#keyholeSprite.rotation = finalRot
+			#keyholeSprite.rotation = startingRotation + (sin(upY) + cos(upX)) / dist * winRotation
+			Input.warp_mouse(rotationHelper.position)
+			
 			print_debug(keyholeSprite.rotation)
 
 		if keyholeSprite.rotation >= winRotation:
 			hasWon = true
+			EventBus.G_UI_MG_LOCK_END.emit()
 			print("won minigame")
 
 
 func _on_input_event(viewport:Node, event:InputEvent, shape_idx:int):
+	# 3
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if canHold:
 			if event.pressed:
@@ -48,6 +83,10 @@ func _on_input_event(viewport:Node, event:InputEvent, shape_idx:int):
 			else:
 				isHeld = false
 	pass # Replace with function body.
+
+	if event is InputEventMouseMotion:
+		upY = event.relative.y
+		upX = event.relative.x
 
 func _on_mouse_entered():
 	canHold = true
