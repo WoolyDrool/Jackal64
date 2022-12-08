df0e424adca2fc403d8508df0b4dc57a4b3011ce
WoolyDrool <nikolai.mcneely15@gmail.com>
2022-12-08 01:08:24 -0800

    Formatted script a bit

--- entities/player/player_controller.cs
+++ entities/player/player_controller.cs
@@ -6,29 +6,57 @@ public partial class player_controller : CharacterBody3D
 	public const float Speed = 5.0f;
 	public const float JumpVelocity = 4.5f;
 
+	[Export] public Camera3D camera;
+	[Export] public Node3D rotationHelper;
+
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
+	// Vectors
+	internal Vector2 _inputDirection;
+	internal Vector3 _controllerDirection;
+
+	public override void _Ready()
+	{
+		//camera = GetTree().Root.GetNode<Camera3D>();
+	}
+
+	public override void _Process(double delta)
+	{
+		_ProcessInput();
+	}
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector3 velocity = Velocity;
-
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.y -= gravity * (float)delta;
 
 		// Handle Jump.
 		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
 			velocity.y = JumpVelocity;
 
 		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Vector3 direction = (Transform.basis * new Vector3(inputDir.x, 0, inputDir.y)).Normalized();
-		if (direction != Vector3.Zero)
+		Velocity = _MoveController(velocity, delta);
+		MoveAndSlide();
+	}
+
+	private void _ProcessInput()
+	{
+
+
+
+		_inputDirection = Input.GetVector("movement_left", "movement_right", "movement_forward", "movement_back");
+	}
+
+	private Vector3 _MoveController(Vector3 velocity, double delta)
+	{
+		// Add the gravity.
+		if (!IsOnFloor())
+			velocity.y -= gravity * (float)delta;
+
+		_controllerDirection = (Transform.basis * new Vector3(_inputDirection.x, 0, _inputDirection.y)).Normalized();
+		if (_controllerDirection != Vector3.Zero)
 		{
-			velocity.x = direction.x * Speed;
-			velocity.z = direction.z * Speed;
+			velocity.x = _controllerDirection.x * Speed;
+			velocity.z = _controllerDirection.z * Speed;
 		}
 		else
 		{
@@ -36,7 +64,6 @@ public partial class player_controller : CharacterBody3D
 			velocity.z = Mathf.MoveToward(Velocity.z, 0, Speed);
 		}
 
-		Velocity = velocity;
-		MoveAndSlide();
+		return velocity;
 	}
 }
